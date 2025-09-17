9b80a919ba04e3710658b6699c9d1d4413f42ed8
HP Pavilion 10 Gen <HP Pavilion 10 Gen@DONET>
2025-09-17 20:13:46 +0400

    ex-Two

---
+++ HwTwo/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace HwTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                //#region 1.რიცხვების ბეჭდვა(Print Numbers 1–10)
+                //for(int q =1; q<=10; q++)
+                //{
+                //    Console.Write(q + " ");
+                //}
+
+                //#endregion
+
+                //#region 2.რიცხვების ჯამი(Sum of Numbers 1–100)
+                //int i = 1;
+                //int sum = 0;
+                //while (i <= 100)
+                //{
+                //    sum += i;
+                //    i++;
+                //}
+                //Console.WriteLine("Sum = " + sum);
+
+                //#endregion
+
+                //#region 3.კენტი რიცხვების ბეჭდვა(Odd Numbers)
+                //int n = 1;
+                //while (n <= 20)
+                //{
+                //    Console.WriteLine(n);
+                //    n += 2;
+                //}
+
+                //#endregion
+
+                //#region  4.რიცხვის ტაბულა(Multiplication Table)
+
+                //Console.Write("Enter Number: ");
+                //int n = int.Parse(Console.ReadLine());
+
+                //for (int i = 1; i <= 9; i++)
+                //{
+                //    Console.WriteLine($"{n} x {i} = {n * i}");
+                //}
+
+
+                //#endregion
+
+                //#region  5.უკუღმა ბეჭდვა(Reverse Numbers) 
+                //int numb = 10;
+                //while (numb >= 1)
+                //{
+                //    Console.WriteLine(numb);
+                //    numb--;
+                //}
+                //#endregion
+
+                //#region  6.რიცხვის ფაქტორიალი(Factorial) 
+                //int k = 1;
+                //int res = 1;
+                //while (k <= 5)
+                //{
+                //    res *= k;
+                //    k++;
+                //}
+                //Console.WriteLine("Res = " + res);
+
+                //#endregion
+
+                //#region  7.სიმბოლოს გამეორება(Repeat Character)
+                //int m = 0;
+                //do
+                //{
+                //    Console.Write("*");
+                //    m++;
+                //} while (m < 10);
+
+                //#endregion
+
+                //#region  8.While ციკლი – ჯამი სანამ არ გადავაჭარბებთ
+                //int summ = 0;
+                //int number = 1;
+
+                //while (summ < 50)
+                //{
+                //    summ += number;
+                //    number++;
+                //}
+
+                //Console.WriteLine("Sum = " + sum);
+                //#endregion
+
+                //#region  9.Do - While ციკლი – პაროლი
+                //Console.WriteLine("Enter password: ");
+                //string pasword= Console.ReadLine();
+                //do
+                //{
+                //    Console.WriteLine("Enter password: ");
+                //    pasword = Console.ReadLine();
+                //} while (pasword != "1234");
+
+                //#endregion
+
+                //#region  10.რიცხვის პალინდრომი(Palindrome Number Check)
+                //Console.WriteLine("Enter a number: ");
+                //int num = int.Parse(Console.ReadLine());
+                //string str = num.ToString();
+                //string reversed = "";
+
+                //foreach (char ch in str.Reverse())
+                //{
+                //    reversed += ch;
+                //}
+
+                //Console.WriteLine(str == reversed ? "Palindrome" : "Not Palindrome");
+
+                //#endregion
+
+                //#region 11.რიცხვების ფიბონაჩის მიმდევრობა(Fibonacci Sequence)
+                //int numbb = 7;
+                //int a = 0, b = 1;
+
+                //Console.Write(a + " " + b + " ");
+
+                //for (int p = 2; p < numbb; p++)
+                //{
+                //    int c = a + b;
+                //    Console.Write(c + " ");
+                //    a = b;
+                //    b = c;
+                //}
+
+
+                //#endregion
+
+            }
+        }
+    }
+
