b07d559d2f1155e4923d714596cd2fdec71ea3b6
HP Pavilion 10 Gen <HP Pavilion 10 Gen@DONET>
2025-10-01 17:43:47 +0400

    მეხუთე დავალების აფდეითი

--- HwFive/Program.cs
+++ HwFive/Program.cs
@@ -1,74 +1,83 @@
-﻿namespace HwFive
+﻿using System;
+
+namespace HwFive
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] numbers1 = { 1, 2, 3, 4, 5 };
-            int[] numbers2 = { 10, 20, 5, 40, 30 };
-            int[] numbers3 = { 1, 2, 2, 3, 1, 4, 2 };
-            int[] arr = { 1, 2, 2, 3, 1 };
+            int[] numbers = GenerateRandomArray(5);
 
-            ReverseArray(numbers1);
-            SearchElement(numbers1);
-            CopyArray(numbers1);
-            FindSecondLargest(numbers2);
-            CountFrequencies(numbers3);
-            RemoveDuplicates(arr);
-        }
+            PrintArray("Original array:", numbers);
 
-        static void ReverseArray(int[] numbers)
-        {
-            Console.WriteLine("Reversed array:");
-            for (int i = numbers.Length - 1; i >= 0; i--)
-            {
-                Console.Write(numbers[i] + " ");
-            }
-            Console.WriteLine();
-        }
+            int[] reversed = ReverseArray(numbers);
+            PrintArray("Reversed array:", reversed);
 
-        static void SearchElement(int[] numbers)
-        {
-            Console.Write("Enter a number to search: ");
-            int n = int.Parse(Console.ReadLine());
-
-            bool found = false;
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                if (numbers[i] == n)
-                {
-                    found = true;
-                    break;
-                }
-            }
-
-            if (found)
-                Console.WriteLine("Number found!");
+            var result = SearchElement(numbers, 3);
+            if (result.found)
+                Console.WriteLine($"Number found at index: {result.index}");
             else
                 Console.WriteLine("Number not found!");
+
+            int[] copy = CopyArray(numbers);
+            PrintArray("Copied array:", copy);
+
+            int secondLargest = FindSecondLargest(numbers);
+            Console.WriteLine($"Second largest number: {secondLargest}");
+
+            CountFrequencies(numbers);
+
+            int[] unique = RemoveDuplicates(numbers);
+            PrintArray("Array without duplicates:", unique);
         }
 
-        static void CopyArray(int[] numbers)
+        static int[] GenerateRandomArray(int size)
+        {
+            Random rnd = new Random();
+            int[] arr = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = rnd.Next(1, 10);
+            }
+            return arr;
+        }
+
+        static int[] ReverseArray(int[] numbers)
+        {
+            int[] reversed = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                reversed[i] = numbers[numbers.Length - 1 - i];
+            }
+            return reversed;
+        }
+
+        static (bool found, int index) SearchElement(int[] numbers, int target)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == target)
+                    return (true, i);
+            }
+            return (false, -1);
+        }
+
+        static int[] CopyArray(int[] numbers)
         {
             int[] copy = new int[numbers.Length];
-
             for (int i = 0; i < numbers.Length; i++)
             {
                 copy[i] = numbers[i];
             }
-
-            Console.WriteLine("Copied array:");
-            for (int i = 0; i < copy.Length; i++)
-            {
-                Console.Write(copy[i] + " ");
-            }
-            Console.WriteLine();
+            return copy;
         }
 
-        static void FindSecondLargest(int[] numbers)
+        static int FindSecondLargest(int[] numbers)
         {
             int max = numbers[0];
-            int secondMax = numbers[0];
+            int secondMax = int.MinValue;
 
             for (int i = 1; i < numbers.Length; i++)
             {
@@ -82,13 +91,12 @@
                     secondMax = numbers[i];
                 }
             }
-
-            Console.WriteLine("Second largest number: " + secondMax);
+            return secondMax;
         }
 
         static void CountFrequencies(int[] numbers)
         {
-            Console.WriteLine("Element frequencies:");
+            Console.WriteLine("Frequencies:");
             for (int i = 0; i < numbers.Length; i++)
             {
                 bool alreadyCounted = false;
@@ -108,35 +116,53 @@
                     for (int j = 0; j < numbers.Length; j++)
                     {
                         if (numbers[j] == numbers[i])
-                        {
                             count++;
-                        }
                     }
-                    Console.WriteLine(numbers[i] + " = " + count);
+                    Console.WriteLine($"{numbers[i]} = {count}");
                 }
             }
         }
 
-        static void RemoveDuplicates(int[] numbers)
+        static int[] RemoveDuplicates(int[] numbers)
         {
-            Console.WriteLine("Array without duplicates:");
+            int[] temp = new int[numbers.Length];
+            int index = 0;
+
             for (int i = 0; i < numbers.Length; i++)
             {
-                bool alreadyPrinted = false;
+                bool exists = false;
 
-                for (int k = 0; k < i; k++)
+                for (int j = 0; j < index; j++)
                 {
-                    if (numbers[i] == numbers[k])
+                    if (numbers[i] == temp[j])
                     {
-                        alreadyPrinted = true;
+                        exists = true;
                         break;
                     }
                 }
 
-                if (!alreadyPrinted)
+                if (!exists)
                 {
-                    Console.Write(numbers[i] + " ");
+                    temp[index] = numbers[i];
+                    index++;
                 }
+            }
+
+            int[] unique = new int[index];
+            for (int i = 0; i < index; i++)
+            {
+                unique[i] = temp[i];
+            }
+
+            return unique;
+        }
+
+        static void PrintArray(string message, int[] arr)
+        {
+            Console.Write(message + " ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
             }
             Console.WriteLine();
         }
